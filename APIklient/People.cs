b93add24a9298a92b3e327a509c5708a26e41693
Mikael <mikael.bergstrom@ga.ntig.se>
2021-02-17 09:47:32 +0100

    Initial commit

---
+++ APIklient/People.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APIklient
+{
+    public class People
+    {
+        public string name;
+        public int height;
+        public string gender;
+    }
+}
---
+++ APIklient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+// RestSharp
+// Newtonsoft Json
+
+namespace APIklient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      RestClient client = new RestClient("https://swapi.dev/api/");
+
+      RestRequest request = new RestRequest("people/12/");
+      string apikey = "strhetbertyhjetyj";
+
+      request.AddParameter("apikey", apikey);
+
+      IRestResponse response = client.Get(request);
+
+      // Console.WriteLine(response.Content);
+
+      // Pokemon charmander = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+      People tarkin = JsonConvert.DeserializeObject<People>(response.Content);
+
+      Console.WriteLine(tarkin.height);
+
+      Console.ReadLine();
+
+    }
+  }
+}
